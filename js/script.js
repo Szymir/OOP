174b5bbffdefa732eebceeb0c7a212837c4ebf84
Szymon Malewski <malewskiszymon@gmail.com>
2018-03-13 20:31:28 +0100

    Add all

--- js/script.js
+++ js/script.js
@@ -5,9 +5,18 @@ $(function(){
 		{brand:"Samsung", price:"3250", color:"black"}
 	];
 
-	console.log(array);
+	function Phone(brand, price, color) {
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+	}
+
+	Phone.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+	}
 
 	for (var i = 0; i < array.length; i++){
-		console.log("The phone brand is " + array[i].brand + ", color is " + array[i].price + " and the price is " + array[i].color + ".");
+		var tel = new Phone(array[i].brand, array[i].price, array[i].color);
+		tel.printInfo();
 	}
 })
